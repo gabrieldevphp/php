4a8aad104035d98ec76a5f7e8bbaa87ad97e485b
gabriel <imleirbag5@gmail.com>
2019-03-04 21:08:30 -0300

    adicionei o praticando.

---
+++ praticando/praticando1.php
@@ -0,0 +1,27 @@
+<?php
+
+$numero1 = 0;
+
+$numero2 = 0;
+
+$res     = 0;
+
+
+ 			
+ 			$numero1 = $_GET ['num1'];
+ 			$numero2 = $_GET ['num2'];
+ 			$op      = $_GET ['tipo'];
+
+
+if ($op == "s") {
+
+     $res = numero1 + numero2;
+
+     echo $res;
+
+} 
+			
+exit()
+
+
+?>
